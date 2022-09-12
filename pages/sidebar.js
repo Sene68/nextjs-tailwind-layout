b89635f22de52d8c2f66c07528e114665a8aee92
Sene68 <senefull68@gmail.com>
2022-09-12 13:49:42 +0900

    contents 형태 잡기

--- pages/sidebar.js
+++ pages/sidebar.js
@@ -8,13 +8,10 @@ export default function Sidebar() {
   return (
     <>
       <BrandSidebarLightHeader>
-        <div className='bg-gray-200'>
+        <div className='bg-gray-200 h-screen'>
           <div className="flex flex-wrap">
-            <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-              1
-            </div>
-            <div className="w-full xl:w-4/12 px-4">
-              2
+            <div className="w-full mb-12 xl:mb-0 px-4">
+              Dashboard
             </div>
           </div>
         </div>

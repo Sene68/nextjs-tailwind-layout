b087976175b6ba4ff05c0e8026d79471c13b88b5
Sene68 <senefull68@gmail.com>
2022-09-13 22:50:25 +0900

    navbar 하단 shadow 추가

--- components/navbar/BrandNavbar.js
+++ components/navbar/BrandNavbar.js
@@ -4,7 +4,7 @@ export default function BrandNavbar() {
   return (
     <>
       {/* Navbar */}
-      <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
+      <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4 bg-white shadow-b">
         <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
           {/* Form */}
           <form className="md:flex hidden flex-row flex-wrap items-center mr-3">

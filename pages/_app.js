80a8d2631e64d7246e9827238a74dbb4914bd13d
Sene68 <senefull68@gmail.com>
2022-09-16 20:07:35 +0900

    dropdown css 사용하지않는 클래스 제거

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../styles/tailwind.css';
-import '../styles/dropdown.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />

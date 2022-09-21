cc78502876f2b15ca496ae74a96e75eab3093e27
Sene68 <senefull68@gmail.com>
2022-09-21 19:51:53 +0900

    dropdown margin top 조절

--- components/dropdown/SimpleDropdown.js
+++ components/dropdown/SimpleDropdown.js
@@ -17,7 +17,7 @@ const SimpleDropdown = () => {
         </span>
       </div>
       <div className={`${isOpen ? 'block' : 'hidden'}`}>
-        <div className="absolute right-0 z-10 mt-8 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
+        <div className="absolute right-0 z-10 mt-7 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
           <div className="py-1" role="none">
               <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-0">Account settings</a>
               <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" id="menu-item-1">Support</a>

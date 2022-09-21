236aaf05ca134acadd0e0e14a795e2f2ad5f16a7
Sene68 <senefull68@gmail.com>
2022-09-21 19:47:43 +0900

    navbar shadow 수정

--- components/navbar/BrandNavbar.js
+++ components/navbar/BrandNavbar.js
@@ -5,7 +5,7 @@ export default function BrandNavbar() {
   return (
     <>
       {/* Navbar */}
-      <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4 bg-white shadow-b">
+      <nav className="absolute top-0 left-0 w-full z-10  md:flex-row md:flex-nowrap md:justify-start flex items-center p-4 bg-white shadow-md">
         <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap px-4">
           {/* Form */}
           <form className="md:flex hidden flex-row flex-wrap items-center mr-3">

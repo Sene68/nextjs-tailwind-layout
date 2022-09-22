1ae8dac8596eeea351016cee4b409072bc80fa7a
Sene68 <senefull68@gmail.com>
2022-09-22 19:47:52 +0900

    main 영역 margin 조정

--- pages/sidebar.js
+++ pages/sidebar.js
@@ -10,8 +10,10 @@ export default function Sidebar() {
       <BrandSidebarLightHeader>
         <div className='bg-gray-200 h-screen'>
           <div className="flex flex-wrap">
-            <div className="font-Montserrat w-full mb-12 xl:mb-0 px-4">
-              Dashboard
+            <div className="w-full m-12 xl:mb-0 px-4">
+              <h2 className="font-Montserrat font-bold text-neutral-900">
+                Dashboard
+              </h2>
             </div>
           </div>
         </div>

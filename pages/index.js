956f284ed4ebd7b9832410e602a698a52cf5a7b0
Sene68 <senefull68@gmail.com>
2022-09-23 18:14:20 +0900

    메인 화면 정리

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -13,42 +14,26 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className="font-Montserrat text-3xl font-bold underline">
-          Hello world!
+          Tailwind Layouts
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <Link href="/sidebar/brand">
+            <a className={styles.card}>
+              <h2 className='font-Montserrat font-bold'>Brand Sidebar &rarr;</h2>
+            </a>
+          </Link>
+          <Link href="/">
+            <a className={styles.card}>
+              <h2 className='font-Montserrat font-bold'>Dark Sidebar &rarr;</h2>
+            </a>
+          </Link>
+          <Link href="/">
+            <a className={styles.card}>
+              <h2 className='font-Montserrat font-bold'>Light Sidebar &rarr;</h2>
+            </a>
+          </Link>
+          
         </div>
       </main>
 

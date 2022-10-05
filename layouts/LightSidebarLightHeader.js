81f20d8bccb4c67e55c6b84a4ed60f88153343f3
Sene68 <senefull68@gmail.com>
2022-10-05 20:37:11 +0900

    light sidebar header를 stack이랑 동일하게 변경

--- layouts/LightSidebarLightHeader.js
+++ layouts/LightSidebarLightHeader.js
@@ -3,15 +3,15 @@ import React from 'react';
 // components
 
 import LightSidebar from "../components/sidebar/LightSidebar.js";
-import BrandNavbar from '../components/navbar/BrandNavbar.js';
+import Navbar from '../components/navbar/Navbar.js';
 
 export default function LightSidebarLightHeader({ children }) {
   return (
     <>
       <LightSidebar />
       <div className="relative ml-64 bg-gray-200">
-        <BrandNavbar />
-        <div className="relative pb-32 pt-12"></div>
+        <Navbar />
+        <div className="relative pb-12 pt-12"></div>
         <div className="mx-auto w-full -m-24">
           {children}
         </div>

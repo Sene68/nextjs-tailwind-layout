15a4e32e6dd7f015199ee3122c746e196d63524f
Sene68 <senefull68@gmail.com>
2022-10-06 19:53:59 +0900

    navbar 코드 정리

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -23,9 +23,7 @@ export default function Navbar() {
     return (
         <>
         <Disclosure as="nav" className="bg-white shadow">
-            {({ open }) => (
-            <>
-                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="px-4 sm:px-6 lg:px-14">
                 <div className="flex h-16 items-center justify-between">
                     <div className="flex items-center">
                         <div className="relative mt-1 rounded-md shadow-sm">
@@ -85,54 +83,8 @@ export default function Navbar() {
                             </Menu>
                         </div>
                     </div>
-                    <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                        <span className="sr-only">Open main menu</span>
-                        {open ? (
-                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                        ) : (
-                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                        )}
-                    </Disclosure.Button>
-                    </div>
                 </div>
-                </div>
-
-                <Disclosure.Panel className="md:hidden">
-                <div className="border-t border-gray-700 pt-4 pb-3">
-                    <div className="flex items-center px-5">
-                    <div className="flex-shrink-0">
-                        <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
-                    </div>
-                    <div className="ml-3">
-                        <div className="text-base font-medium leading-none text-white">{user.name}</div>
-                        <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
-                    </div>
-                    <button
-                        type="button"
-                        className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    >
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                    </div>
-                    <div className="mt-3 space-y-1 px-2">
-                    {userNavigation.map((item) => (
-                        <Disclosure.Button
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                        >
-                        {item.name}
-                        </Disclosure.Button>
-                    ))}
-                    </div>
-                </div>
-                </Disclosure.Panel>
-            </>
-            )}
+            </div>
         </Disclosure>
     </>
     )

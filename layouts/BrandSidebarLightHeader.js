41c8638a8bb6b48ea525a92acfb3a6379a41de9f
Sene68 <senefull68@gmail.com>
2022-10-07 19:41:26 +0900

    sidebar의 header 변경

--- layouts/BrandSidebarLightHeader.js
+++ layouts/BrandSidebarLightHeader.js
@@ -3,14 +3,14 @@ import React from 'react';
 // components
 
 import BrandSidebar from "../components/sidebar/BrandSidebar.js";
-import BrandNavbar from '../components/navbar/BrandNavbar.js';
+import Navbar from '../components/navbar/Navbar.js';
 
 export default function BrandSidebarLightHeader({ children }) {
   return (
     <>
       <BrandSidebar />
       <div className="relative ml-64 bg-gray-200">
-        <BrandNavbar />
+        <Navbar />
         <div className="relative pb-32 pt-12"></div>
         <div className="mx-auto w-full -m-24">
           {children}
--- layouts/DarkSidebarLightHeader.js
+++ layouts/DarkSidebarLightHeader.js
@@ -3,14 +3,14 @@ import React from 'react';
 // components
 
 import DarkSidebar from "../components/sidebar/DarkSidebar.js";
-import BrandNavbar from '../components/navbar/BrandNavbar.js';
+import Navbar from '../components/navbar/Navbar.js';
 
 export default function BrandSidebarLightHeader({ children }) {
   return (
     <>
       <DarkSidebar />
       <div className="relative ml-64 bg-gray-200">
-        <BrandNavbar />
+        <Navbar />
         <div className="relative pb-32 pt-12"></div>
         <div className="mx-auto w-full -m-24">
           {children}
